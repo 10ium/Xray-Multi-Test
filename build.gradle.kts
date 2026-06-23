e156e0538c0a68d07e7ab5b59a29c0c770450088
Tenium <amin.behravan@yahoo.com>
2026-06-23 21:44:28 +0330

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// فایل کانفیگ سطح پروژه ریشه (Top-level) جهت معرفی نسخه‌های پلاگین‌های کامپایلر به تمام ماژول‌ها
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
